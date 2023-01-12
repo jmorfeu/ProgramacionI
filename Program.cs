e1fec540355dfe7758fdedc0b440b198a8bd3ef7
Joerlyn Morfe <joelyn_2088@hotmail.com>
2023-01-11 22:11:05 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            
+            Console.WriteLine("Indique el primer numero: ");
+            int valor1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Indique el segundo numero: ");
+            int valor2 = Convert.ToInt32(Console.ReadLine());
+
+            Suma suma = new Suma(valor1,valor2);
+            
+            Console.WriteLine($"La Resta es: {suma.ResultadoResta()}");
+            Console.WriteLine($"La suma es: {suma.ResultadoSuma()}");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Suma.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Suma
+    {
+        private int Valor1 { get; set; }
+        private int Valor2 { get; set; }
+
+        public Suma(int valor1, int valor2)
+        {
+            this.Valor1 = valor1;
+            this.Valor2 = valor2;
+        }
+
+
+        public int ResultadoSuma()
+        {
+            return Valor1 + Valor2;
+        }
+
+        public int ResultadoResta()
+        {
+            return Valor1 - Valor2;
+        }
+    }
+}
